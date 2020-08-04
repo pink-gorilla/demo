d9582266a8c74c8d8c297e69a60c5b45b9595ea4
CI/CD <ci@pinkgorilla.org>
2020-08-04 15:16:34 +0000

    Release 0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/demo-goldly "0.0.2-SNAPSHOT"
+(defproject org.pinkgorilla/demo-goldly "0.0.2"
   :description "goldly demo"
   :license {:name "MIT"}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
