5374e2360b4b002bb5a4fbc3bb7730e2cd9ad257
CI/CD <ci@pinkgorilla.org>
2021-05-05 16:37:48 +0000

    Release 0.2.28

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/goldly-server "0.2.28-SNAPSHOT"
+(defproject org.pinkgorilla/goldly-server "0.2.28"
   :description "goldly server"
   :license {:name "MIT"}
   :url "https://github.com/pink-gorilla/goldly-server"
