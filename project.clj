8682153d26da618c52cf4f1c62c9b1b9b3aed0e9
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-05-06 18:13:49 -0500

    dep bump

--- project.clj
+++ project.clj
@@ -32,7 +32,7 @@
   ;                 #"nrepl.*"
   ;                 #"taoensso.*"]
 
-  :dependencies [[org.pinkgorilla/goldly "0.2.27"]
+  :dependencies [[org.pinkgorilla/goldly "0.2.29"]
                  ; bundled dependencies
                  [org.pinkgorilla/gorilla-ui "0.3.18" :exclusions [org.clojure/clojurescript]]
                  [org.pinkgorilla/gorilla-plot "1.2.6" :exclusions [org.clojure/clojurescript]]]
