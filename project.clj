6a097b38712ff64f137739185b4ef5dbe1d6587e
CI/CD <ci@pinkgorilla.org>
2021-05-19 22:15:04 +0000

    Release 0.2.34

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/goldly-bundel "0.2.34-SNAPSHOT"
+(defproject org.pinkgorilla/goldly-bundel "0.2.34"
   :description "goldly bundel"
   :license {:name "MIT"}
   :url "https://github.com/pink-gorilla/goldly-bundel"
